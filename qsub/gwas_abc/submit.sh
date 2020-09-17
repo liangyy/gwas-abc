caba7a30c67adc3c284da0d7ecbd348ced98319a
Yanyu Liang <yanyul@uchicago.edu>
2020-09-17 17:53:25 -0500

    added submission script

---
+++ qsub/gwas_abc/submit.sh
@@ -0,0 +1,9 @@
+# args1: middle name of config file
+# args2 (optional): preprocessing script (to generate GWAS table)
+
+if [[ -z $2 ]]
+then
+  qsub -v CONFIG=$1,PREP=$2 run.qsub
+else
+  qsub -v CONFIG=$1 run.qsub
+fi
